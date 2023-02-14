6a5b8dd0f983926590800506255d592d55725b2d
ciaomin <115628418+ciaomin@users.noreply.github.com>
2023-02-14 17:08:52 +0900

    Add files via upload

---
+++ DadaicksunApplication.java
@@ -0,0 +1,13 @@
+package Dadaicksun.pre_pr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadaicksunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadaicksunApplication.class, args);
+	}
+
+}
